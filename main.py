6d21ef65a01cf76b236473045abd90c6aedf45b8
Gabriel Gularte <gabrielfgularte@gmail.com>
2022-06-11 01:32:21 -0300

    Add around behavior

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ def paginate(total_pages, current_page, boundaries, around):
     all_pages = list(range(1, total_pages + 1))
 
     for page in all_pages:
-        if page <= boundaries or page > total_pages - boundaries or page == current_page:
+        if page <= boundaries or page > total_pages - boundaries:
             pages.append(page)
+            continue
+
+        if (page >= current_page - around and
+                page < current_page or
+                current_page + around >= page and
+                page >= current_page):
+            pages.append(page)
+        elif pages[-1] != '...':
+            pages.append('...')
 
     return pages
 
 if __name__ == '__main__':
-    paginate(5, 4, 1, 0)
+    print('Example 1:', paginate(5, 4, 1, 0))
+    print('Example 2:', paginate(10, 4, 2, 2))
