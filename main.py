87dec96268a89c6a106da0782bf7e1862419af58
Gabriel Gularte <gabrielfgularte@gmail.com>
2022-06-11 01:23:19 -0300

    Add boundaries

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def paginate(total_pages, current_page=1, boundaries=0, around=0):
-    return []
+def paginate(total_pages, current_page, boundaries, around):
+
+    pages = list()
+    all_pages = list(range(1, total_pages + 1))
+
+    for page in all_pages:
+        if page <= boundaries or page > total_pages - boundaries or page == current_page:
+            pages.append(page)
+
+    return pages
+
+if __name__ == '__main__':
+    paginate(5, 4, 1, 0)
--- tests.py
+++ tests.py
@@ -5,11 +5,29 @@ from main import paginate
 
 class PaginatorTestCase(unittest.TestCase):
 
+    def test_boundaries(self):
+        result = paginate(
+            total_pages=5,
+            current_page=4,
+            boundaries=1,
+            around=0
+        )
+        self.assertEqual(result, [1, 4, 5])
+
+        result = paginate(
+            total_pages=50564,
+            current_page=4765,
+            boundaries=3,
+            around=0
+        )
+        self.assertEqual(result, [1, 2, 3, 4765, 50562, 50563, 50564])
+
     def test_pagination(self):
         result = paginate(
             total_pages=5,
             current_page=4,
-            boundaries=1
+            boundaries=1,
+            around=0
         )
         self.assertEqual(result, [1, '...', 4, 5])
 
